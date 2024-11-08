ca10fb6ba4b3a90264e7ce6c7c1462bc81d83876
aqzholsky <98736657+aqzholsky@users.noreply.github.com>
2024-11-08 05:23:53 +0500

    Update urls.py

--- app/project/urls.py
+++ app/project/urls.py
@@ -26,10 +26,14 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls", namespace="api")),
-    path("api/schema/v1/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "swagger",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("api/schema/v1/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "swagger",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+    ]
