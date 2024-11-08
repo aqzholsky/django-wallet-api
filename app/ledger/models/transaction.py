e123a7842711ba8ef54e8e9741588e0843a80dd4
aqzholsky <98736657+aqzholsky@users.noreply.github.com>
2024-11-08 05:03:01 +0500

    index added

--- app/ledger/models/transaction.py
+++ app/ledger/models/transaction.py
@@ -7,5 +7,5 @@ class Transaction(TimestampMixin, models.Model):
     wallet = models.ForeignKey(
         "ledger.Wallet", on_delete=models.PROTECT, related_name="transactions"
     )
-    txid = models.CharField(max_length=255, unique=True)
+    txid = models.CharField(max_length=255, unique=True, db_index=True)
     amount = models.DecimalField(max_digits=36, decimal_places=18)
